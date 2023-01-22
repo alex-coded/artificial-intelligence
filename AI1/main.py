f9db935f61c41c463270bde0e999cc09381fd348
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2023-01-22 15:21:23 +0200

    .

--- AI1/main.py
+++ AI1/main.py
@@ -11,7 +11,17 @@ path_bck = []
 visited_bfs = {}
 solution_bfs = -1
 path_bfs = {}
-queue = deque()
+queue = []
+
+visited_hill = {}
+solution_hill = -1
+path_hill = {}
+queue_hill = []
+
+visited_a_star = {}
+solution_a_star = -1
+path_a_star = {}
+queue_a_star = []
 
 
 def get_initial_state(k, capacity1, capacity2):
@@ -135,6 +145,11 @@ def print_path(state):
         print(state)
 
 
+def print_path_a_star(state):
+    if (state[1][1], state[2][1]) in path_a_star:
+        print_path_a_star(path_a_star[(state[1][1], state[2][1])])
+
+
 def breadth_first_search(state):
     """
 
@@ -143,7 +158,6 @@ def breadth_first_search(state):
     """
     global visited_bfs
     global solution_bfs
-    global path
     global queue
     global path_bfs
 
@@ -193,48 +207,155 @@ def breadth_first_search(state):
             print("no solution")
 
 
-def improvement_func(state):
-    return 1 - (abs(state[0] - state[1][1]) + abs(state[0] - state[2][1])) / state[0]
+def heuristic(state):
+    return (abs(state[1][1] - state[0])) * (abs(state[2][1] - state[0]))
 
-global heur_state
+
+def heuristic2(state):
+    return (abs(state[1][1] - state[0])) * (1 / state[1][0]) + (abs(state[2][1] - state[0])) * (1 / state[2][0])
+
+
+def improvement_func(state):
+    return abs(1 - (abs(state[0] - state[1][1]) + abs(state[0] - state[2][1])) / state[0])
+
+
+def check_if_transition_successful_hill(is_successful, new_state):
+    global visited_hill
+    if is_successful:
+        if ((new_state[1][1], new_state[2][1]) not in visited_hill):
+            return True
+        elif visited_hill[(new_state[1][1], new_state[2][1])] < 6:
+            return True
+    return False
+
 
 def hill_climbing(state):
-    arr_solutions = list()
-    while True:
-        operation = random.randint(0, 9)
+    """
+
+    :param state: the state passed as parameter
+    :return:
+    """
+    global visited_hill
+    global solution_hill
+    global queue_hill
+    global path_hill
+
+    if ((state[1][1], state[2][1]) not in visited_hill):
+        visited_hill[(state[1][1], state[2][1])] = 1
+    else:
+        visited_hill[(state[1][1], state[2][1])] += 1
+
+    if solution_hill == -1:
         for jug_index in range(1, 3):
-            if operation == 0:
-                is_successful, new_state = fill_transition(state, jug_index)
-                if is_successful:
-                    arr_solutions.append(new_state)
+            # fill transition
+            is_successful, new_state = fill_transition(state, jug_index)
+            if check_if_transition_successful_hill(is_successful, new_state):
+                queue_hill.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_hill:
+                    path_hill[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_hill = new_state
+                        print_path(new_state)
+                        exit()
 
-            elif operation == 1:
-                is_successful, new_state = pour_transition(state, jug_index)
-                if is_successful:
-                    arr_solutions.append(new_state)
-            else:
-                is_successful, new_state = empty_transition(state, jug_index)
-                if is_successful:
-                    arr_solutions.append(new_state)
+            # pour transition
+            is_successful, new_state = pour_transition(state, jug_index)
+            if check_if_transition_successful_hill(is_successful, new_state):
+                queue_hill.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_hill:
+                    path_hill[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_hill = new_state
+                        print_path(new_state)
+                        exit()
 
-        best_fit = -1
-        best_state = state
-        heuristic_results = list()
-        for elm in arr_solutions:
-            # print(elm)
-            # heuristic_results[tuple(elm)] = improvement_func(elm)
-            # heuristic_results[tuple(elm)] = improvement_func(elm)
-            # best
-            heuristic_results.append(tuple((elm[1][1], elm[2][1])))
+            # empty transition
+            is_successful, new_state = empty_transition(state, jug_index)
+            if check_if_transition_successful_hill(is_successful, new_state):
+                queue_hill.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_hill:
+                    path_hill[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_hill = new_state
+                        print_path(new_state)
+                        exit()
 
-        for key, value in heuristic_results:
-            if value < best_fit:
-                best_fit = value
-                best_state = key
+        # print("path", path)gf
+        queue_hill.pop(0)
+        if len(queue_hill) > 0:
+            queue_hill.sort(key=heuristic2)
+            print('Test:')
+            print(state)
+            print(heuristic2(state), heuristic2(queue_hill[0]))
+            if heuristic2(state) >= heuristic2(queue_hill[0]):
+                future_state = queue_hill[0]
+                queue_hill.clear()
+                hill_climbing(future_state)
+        else:
+            print("No solution")
 
-        state = best_state
-        print(state)
-        # print(best_fit)
+
+def check_if_transition_successful_a_star(is_successful, new_state):
+    global visited_a_star
+    if is_successful and ((new_state[1][1], new_state[2][1]) not in visited_a_star):
+        visited_a_star[(new_state[1][1], new_state[2][1])] = 1
+        return True
+    return False
+
+
+def a_star(state):
+    """
+    :param state: the state passed as parameter
+    :return:
+    """
+    global visited_a_star
+    global solution_a_star
+    global queue
+    global path_a_star
+
+    if state not in queue:
+        queue.append(state)
+
+    if solution_a_star == -1:
+        for jug_index in range(1, 3):
+            # fill transition
+            is_successful, new_state = fill_transition(state, jug_index)
+            if check_if_transition_successful_a_star(is_successful, new_state):
+                queue.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_a_star:
+                    path_a_star[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_a_star = new_state
+                        print_path_a_star(new_state)
+                        exit()
+
+            # pour transition
+            is_successful, new_state = pour_transition(state, jug_index)
+            if check_if_transition_successful_a_star(is_successful, new_state):
+                queue.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_a_star:
+                    path_a_star[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_a_star = new_state
+                        print_path_a_star(new_state)
+                        exit()
+
+            # empty transition
+            is_successful, new_state = empty_transition(state, jug_index)
+            if check_if_transition_successful_a_star(is_successful, new_state):
+                queue.append(new_state)
+                if (new_state[1][1], new_state[2][1]) not in path_a_star:
+                    path_a_star[(new_state[1][1], new_state[2][1])] = state
+                    if is_final_state(new_state):
+                        solution_a_star = new_state
+                        print_path_a_star(new_state)
+                        exit()
+        queue.pop(0)
+        if len(queue) > 0:
+            queue.sort(key=heuristic)
+            a_star(queue[0])
+        else:
+            print("no solution")
 
 
 def water_jug_problem(k, capacity1, capacity2):
@@ -248,12 +369,20 @@ def water_jug_problem(k, capacity1, capacity2):
     # visited[(0, 0)] = 1
     # backtracking(state)
     # print(solution)
-    #
+
     # print('BFS')
     # visited_bfs[(0, 0)] = 1
     # breadth_first_search(state)
 
+    print('hill_climbing')
+    visited_hill[(0, 0)] = 1
     hill_climbing(state)
+    if solution_hill == -1:
+        print('Euristica s-a blocat intr-un maxim local, dar exista o solutie')
+
+    # print('A star')
+    # visited_a_star[(0, 0)] = 1
+    # a_star(state)
 
 
 if __name__ == '__main__':
